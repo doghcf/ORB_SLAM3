b4c8a0d5fc37efbde585e6eaf2de847f38716c64
doghcf <522563047@qq.com>
2025-03-24 22:23:53 +0800

    1

---
+++ run_kitti_stereo_inertial.sh
@@ -0,0 +1,6 @@
+./Examples/Stereo-Inertial/stereo_inertial_kitti \
+./Vocabulary/ORBvoc.txt \
+./Examples/Stereo-Inertial/KITTI04-12.yaml \
+/home/fu/dataset/kitti/data_odometry_gray/kitti/dataset/sequences/07 \
+/home/fu/dataset/kitti/raw_data/2011_09_30/2011_09_30_drive_0027_extract/oxts/data \
+/home/fu/dataset/kitti/data_odometry_gray/kitti/dataset/sequences/07/times_imu100hz_07.txt
